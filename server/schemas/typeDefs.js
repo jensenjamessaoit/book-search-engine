bd0fe3628e04569a9764c7c67c7784df04355c56
jj <saoitjensen@gmail.com>
2024-01-26 18:25:34 -0700

    changed to MERN

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,21 @@
+const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Book {
+        _id: ID
+        authors: String
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+`;
+
+module.exports = typeDefs;
