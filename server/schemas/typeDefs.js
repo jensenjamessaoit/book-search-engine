d88c3a5aef6b3c2be4128f7429411649d53c9c7c
jj <saoitjensen@gmail.com>
2024-02-14 23:08:03 -0700

    typeDef Mutations and Query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,47 @@
-const typeDefs = `
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        password: String!
-        savedBooks: [Book]
-    }
+const gql = require("graphql-tag");
 
-    type Book {
-        _id: ID
-        authors: String
-        description: String!
-        bookId: String!
-        image: String
-        link: String
-        title: String!
-    }
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
