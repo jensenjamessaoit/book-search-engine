c91297b6ea4b3cf274bae95ed44bee8c21950376
jj <saoitjensen@gmail.com>
2024-02-15 20:54:09 -0700

    auth and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("savedBooks");
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {},
+};
