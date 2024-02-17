8bf310f1f2f3706d8282d5a7c49fd858e0dcb90c
jj <saoitjensen@gmail.com>
2024-02-16 20:17:00 -0700

    mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,5 +11,30 @@ const resolvers = {
     },
   },
 
-  Mutation: {},
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
+
+module.exports = resolvers;
